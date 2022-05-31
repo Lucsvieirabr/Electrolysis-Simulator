40adba40e3f53057dd9a7a4b4dae298240d28d8b
FIRST_NAME LAST_NAME <62450294+Lucsvieirabr@users.noreply.github.com>
2022-05-31 16:08:19 -0300

    Get OxValue

--- main.js
+++ main.js
@@ -1,13 +1,24 @@
 function request_API(element_name){
     return new Request('https://periodic-table-elements-info.herokuapp.com/element/symbol/'+element_name);
 }
-async function get_API_JSON(){
-    return await fetch(request_API()).then(response => response.json()).then(json =>{
-        return json;
+
+async function get_Element_JSON(element){
+    return await fetch(request_API(element)).then(response => response.json()).then(json =>{
+        return json[0];
     });
 }
-async function teste(){
-    return await get_API_JSON();
+async function get_Cation_Info(){
+     return await get_Element_JSON(document.getElementById('cation').value);
+ }
+
+async function get_Anion_Info(){
+    return await get_Element_JSON(document.getElementById('anion').value);
 }
-let periodicTable = teste();
-console.log(periodicTable);
+
+async function teste(){
+    let cation_info = await get_Cation_Info();
+    let radius = cation_info.ionRadius.slice(-3, -1);
+    console.log(radius);
+    console.log(get_Cation_Info());
+    console.log(get_Anion_Info());
+}
